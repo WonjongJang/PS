e0756717e09a576dc8595974462bc3df5c421316
won <87519450+wonjongjang@users.noreply.github.com>
2024-08-27 10:45:35 +0900

    Time: 49 ms (67.88%), Space: 49.2 MB (14.85%) - LeetHub

---
+++ LeetCode/0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    let mid = head
+    let end = head
+
+    while (end && end.next) {
+        mid = mid.next
+        end = end.next.next
+    }
+
+    return mid
+};
