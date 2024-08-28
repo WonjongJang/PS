e622241eb656f89a1daca2ca313c7695afd6da92
won <87519450+wonjongjang@users.noreply.github.com>
2024-08-28 09:08:31 +0900

    Time: 85 ms (17.22%), Space: 52.4 MB (69.7%) - LeetHub

--- LeetCode/0383-ransom-note/0383-ransom-note.js
+++ LeetCode/0383-ransom-note/0383-ransom-note.js
@@ -4,5 +4,23 @@
  * @return {boolean}
  */
 var canConstruct = function(ransomNote, magazine) {
-    return magazine.includes(ransomNote)
+    let map = new Map();
+
+    for (m of magazine) {
+        if (map.get(m)) {
+            map.set(m, map.get(m) + 1)
+        } else {
+            map.set(m, 1)
+        }
+    }
+
+    for (r of ransomNote) {
+        if (map.get(r)) {
+            map.set(r, map.get(r) - 1)
+        } else {
+            return false
+        }
+    }
+
+    return true
 };
