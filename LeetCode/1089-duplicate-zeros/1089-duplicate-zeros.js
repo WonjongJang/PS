d29f731948a5f9eaa1ac94531e7232df91ed533b
won <87519450+wonjongjang@users.noreply.github.com>
2024-08-29 13:54:27 +0900

    Time: 78 ms (18.66%), Space: 51.8 MB (47.23%) - LeetHub

---
+++ LeetCode/1089-duplicate-zeros/1089-duplicate-zeros.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} arr
+ * @return {void} Do not return anything, modify arr in-place instead.
+ */
+var duplicateZeros = function(arr) {
+    let i = 0
+    while (i < arr.length) {
+        if (arr[i] === 0) {
+            for (let j = arr.length-1; j > i; j--) {
+                arr[j] = arr[j-1]
+            }
+            i += 2
+        } else {
+            i += 1
+        }
+    }
+    
+    return arr
+};
