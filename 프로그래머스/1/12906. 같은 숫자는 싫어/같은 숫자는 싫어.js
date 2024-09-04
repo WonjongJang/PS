b9159b970aab7e4d8654e4fa73a01f426ff1fe15
won <87519450+wonjongjang@users.noreply.github.com>
2024-09-04 10:48:50 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 59.95 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,10 +1,18 @@
 function solution(arr) {
 	const answer = [];
 	for (let i = 0; i < arr.length; i++) {
-        if (answer.at(-1) !== arr[i]) {
-            answer.push(arr[i])
-        }
+		if (answer.at(-1) !== arr[i]) {
+			answer.push(arr[i]);
+		}
 	}
-    
+
 	return answer;
+
+	//     for (a of arr) {
+	//         if (answer.at(-1) !== a) {
+	//             answer.push(a)
+	//         }
+	//     }
+
+	// 	return answer;
 }
