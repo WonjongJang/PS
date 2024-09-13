491636adda22f36d14c4a99108286e5e69547c8b
won <87519450+wonjongjang@users.noreply.github.com>
2024-09-13 14:02:56 +0900

    Time: 50 ms (81.74%), Space: 50 MB (44.14%) - LeetHub

--- LeetCode/1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.js
+++ LeetCode/1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.js
@@ -3,14 +3,12 @@
  * @return {boolean}
  */
 var checkIfExist = function (arr) {
-	// 내림차순으로 큰 요소 부터 보게 되면 이후 요소의 2배가 있는지만 확인하면 됨
-	// 음수인 경우에 대비해 절대값으로 변환
-	const newArr = arr.map(v => Math.abs(v)).sort((a, b) => b - a);
-
 	const hash = new Map();
-	for (let i = 0; i < newArr.length; i++) {
-		if (hash.has(newArr[i] * 2)) return true;
-		hash.set(newArr[i], i);
+	for (let i = 0; i < arr.length; i++) {
+		if (hash.has(arr[i])) return true;
+		// hash에 현재 요소의 2배와 1/2배를 모두 저장 (1/2배는 짝수인 경우만)
+		hash.set(arr[i] * 2, i);
+		if (arr[i] % 2 === 0) hash.set(arr[i] / 2, i);
 	}
 
 	return false;
