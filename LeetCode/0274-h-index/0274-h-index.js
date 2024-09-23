369ddfbcf73c0fdc6b61407ed97e6738f998ff09
won <87519450+wonjongjang@users.noreply.github.com>
2024-09-23 09:45:23 +0900

    Time: 53 ms (66.62%), Space: 48.7 MB (89.74%) - LeetHub

---
+++ LeetCode/0274-h-index/0274-h-index.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} citations
+ * @return {number}
+ */
+var hIndex = function (citations) {
+	citations.sort((a, b) => b - a); // 내림차순 정렬
+
+	let maxValue = 0; // H-Index 중 최대값
+	// H-Index: a번 이상 인용된 논문이 b편 이상일 때 a 와 b 중 작은 값
+	for (let i = 0; i < citations.length; i++) {
+		maxValue = Math.max(maxValue, Math.min(i + 1, citations[i]));
+	}
+
+	return maxValue;
+};
