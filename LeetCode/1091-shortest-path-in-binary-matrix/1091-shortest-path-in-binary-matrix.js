2b0b06610347749a6b822bac64346d10a1339753
won <87519450+wonjongjang@users.noreply.github.com>
2024-09-25 18:35:53 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.js
+++ LeetCode/1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.js
@@ -2,31 +2,36 @@
  * @param {number[][]} grid
  * @return {number}
  */
-var shortestPathBinaryMatrix = function(grid) {
-    const n = grid.length
-    if (grid[0][0] || grid[n-1][n-1]) return -1
+var shortestPathBinaryMatrix = function (grid) {
+	const n = grid.length; // n x n 행렬
+	if (grid[0][0] || grid[n - 1][n - 1]) return -1; // 출발점 또는 도착점이 1인 경우
 
-    const visited = Array.from({ length: n }, () => Array(n).fill(0));
-    
-    const dx = [1, 0, -1, 0, 1, 1, -1, -1]
-    const dy = [0, -1, 0, 1, 1, -1, 1, -1]
+	const visited = Array.from({ length: n }, () => Array(n).fill(0)); // 방문 기록 및 카운트
 
-    const que = [[0, 0, 1]]
-    visited[0][0] = 1
-    while (que.length) {
-        const [x, y, cnt] = que.shift()
-        
-        for (let d = 0; d < 8; d++) {
-            const nx = x + dx[d]
-            const ny = y + dy[d]
+	// 방향벡터 (대각선 이동 포함)
+	const dx = [1, 0, -1, 0, 1, 1, -1, -1];
+	const dy = [0, -1, 0, 1, 1, -1, 1, -1];
 
-            if (0 <= nx && nx < n && 0 <= ny && ny < n && grid[nx][ny] === 0 && visited[nx][ny] === 0) {
-                que.push([nx, ny, cnt+1])
-                visited[nx][ny] = cnt+1
-            }
-        }
-    }
+	const que = [[0, 0]];
+	visited[0][0] = 1;
+	while (que.length) {
+		const [x, y] = que.shift();
 
-    console.log(visited)
-    return visited[n-1][n-1] ? visited[n-1][n-1] : -1
+		for (let d = 0; d < 8; d++) {
+			const nx = x + dx[d];
+			const ny = y + dy[d];
+
+			// 도착점인 경우
+			if (nx === n - 1 && ny === n - 1) {
+				return visited[x][y] + 1;
+			}
+
+			if (0 <= nx && nx < n && 0 <= ny && ny < n && grid[nx][ny] === 0 && visited[nx][ny] === 0) {
+				que.push([nx, ny, visited[x][y] + 1]);
+				visited[nx][ny] = visited[x][y] + 1;
+			}
+		}
+	}
+
+	return -1; // 도착점에 도착하지 못한 경우
 };
